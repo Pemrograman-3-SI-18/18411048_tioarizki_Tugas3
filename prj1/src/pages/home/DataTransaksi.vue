9d6903160863002b77d271939c2ea429d70df01a
TioArizki <tio.18411048@student.ubl.ac.id>
2020-04-14 11:36:40 +0700

    Tugas3

---
+++ prj1/src/pages/home/DataTransaksi.vue
@@ -0,0 +1,103 @@
+<template>
+  <q-page>
+    <div class="q-pa-md">
+      <q-table
+        title="Treats"
+        :data="data"
+        :columns="columns"
+        row-key="id"
+        :filter="filter"
+        :loading="loading"
+      >
+
+        <template v-slot:top>
+          <span class="text-h5 text-weight-light q-pa-md">
+        <span class="text-blue-grey-14">Data Transaksi</span>
+      </span>
+          <!--<q-btn class="q-ml-sm" color="primary" :disable="loading" label="Remove row" @click="removeRow" />-->
+          <q-space />
+          <q-input borderless dense debounce="300" color="primary" v-model="filter">
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </template>
+
+      </q-table>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: false,
+      filter: '',
+      rowCount: 10,
+      columns: [
+        {
+          name: 'desc',
+          required: true,
+          label: 'Kode Transaksi',
+          align: 'left',
+          field: row => row.kodeTransaksi,
+          format: val => `${val}`,
+          sortable: true
+        },
+        { name: 'namaPembeli', align: 'center', label: 'Nama Pembeli', field: 'namaPembeli', sortable: true },
+        { name: 'judulBuku', align: 'center', label: 'Judul Buku', field: 'judulBuku', sortable: true },
+        { name: 'hargaBuku', label: 'Harga Buku', align: 'center', field: 'hargaBuku' },
+        { name: 'jumlahBeli', label: 'Jumlah Beli', align: 'center', field: 'jumlahBeli' },
+        { name: 'total', label: 'Tiotal', align: 'center', field: 'total' }
+      ],
+      data: [
+        {
+          kodeTransaksi: 'TR-001',
+          namaPembeli: 'Jono',
+          judulBuku: 'Pemrograman 3',
+          hargaBuku: '50000',
+          jumlahBeli: '3',
+          total: '105000'
+        },
+        {
+          kodeTransaksi: 'TR-002',
+          namaPembeli: 'Joni',
+          judulBuku: 'Kiat Sukses',
+          hargaBuku: '45000',
+          jumlahBeli: '2',
+          total: '90000'
+        }
+      ]
+    }
+  },
+
+  methods: {
+    // emulate fetching data from server
+    addRow () {
+      this.loading = true
+      setTimeout(() => {
+        const
+          index = Math.floor(Math.random() * (this.data.length + 1)),
+          row = this.original[Math.floor(Math.random() * this.original.length)]
+        if (this.data.length === 0) {
+          this.rowCount = 0
+        }
+        row.id = ++this.rowCount
+        const addRow = { ...row } // extend({}, row, { name: `${row.name} (${row.__count})` })
+        this.data = [...this.data.slice(0, index), addRow, ...this.data.slice(index)]
+        this.loading = false
+      }, 500)
+    },
+
+    removeRow () {
+      this.loading = true
+      setTimeout(() => {
+        const index = Math.floor(Math.random() * this.data.length)
+        this.data = [...this.data.slice(0, index), ...this.data.slice(index + 1)]
+        this.loading = false
+      }, 500)
+    }
+  }
+}
+</script>
